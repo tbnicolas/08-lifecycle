807cef3d6b60a715fc47783ec80a216112a980e9
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-05-08 17:08:10 -0500

    Angular Hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuestraNombreComponent,
+    Pagina1Component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,65 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component
+ implements OnInit, OnChanges, DoCheck , AfterContentInit, AfterContentChecked,
+            AfterViewInit, AfterViewChecked,OnDestroy{
+
+  nombre: string  = 'nico';
+
+  segundos: number = 0;
+
+  timerSubscription!: Subscription;
+
+  constructor() {
+    console.log('constructor');
+  }
+
+
+  ngOnInit(): void {
+    //llamar peticiones asyncronas
+    console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe(i=>{
+      this.segundos = i;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('Timer limpiado')
+
+  }
+
+
+  guardar() {
+
+  }
+
+
+
+}
